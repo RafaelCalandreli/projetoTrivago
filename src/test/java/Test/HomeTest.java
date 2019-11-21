b6864f846166e6429a64dbe445c75ad59a4fcd6a
Rafael Calandreli Oliveira Araújo <rcalandreli@gmail.com>
2019-11-21 11:36:24 -0300

    Alteracoes na classe de Testes e na auxiliar

--- src/test/java/Test/HomeTest.java
+++ src/test/java/Test/HomeTest.java
@@ -1,13 +1,9 @@
 package Test;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import page.HomePage;
@@ -20,9 +16,6 @@ public class HomeTest {
 	private WebDriverWait wait;
 	private String idTC;
 	private HomePage page;
-	private String password = "161616";
-	private String user = "rcalandreliwork@gmail.com";
-	private String mensagem;
 
 	@Before
 	public void setupBeforeTest() {
@@ -41,16 +34,10 @@ public class HomeTest {
 
 	@After
 	public void setupAfterTest() {
-	//	 driver.quit();
+		 driver.quit();
 	}
 
-	/*
-	 *  1. Acessar o site http://www.trivago.com.br
-	 *  2. Digitar o valor “Natal” no  campo de busca 
-	 *  3. Clicar no botão "Pesquisar" 
-	 *  4. Selecionar a opção *  "Quarto Individual"
-	 *   5. Selecionar a opção "Ordenar apenas por Distância"
-	 */
+
 	@Test
 	public void testandofluxoPrincipal() throws InterruptedException {
 		page.preencherCidade("Natal");
--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -1,25 +1,15 @@
 package page;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import suport.Library;
-
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Before;
 
 public class HomePage {
 
@@ -27,7 +17,6 @@ public class HomePage {
 	private Library lib;
 	private WebDriverWait wait;
 	private String idTC;
-	private String url = "https://bancodeseries.com.br/";
 
 	public HomePage(WebDriver driver, Library lib, WebDriverWait wait, String idTC) {
 		super();
